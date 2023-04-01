c0aff71f5b78fbf8e4aa2e0e160ca772d8501c08
Dive57 <127876528+Dive57@users.noreply.github.com>
2023-04-01 15:29:21 +0300

    Add files via upload

---
+++ homework/task_6/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("enter the number");
+        int number = (int)Convert.ToUInt32(Console.ReadLine());
+
+        int remDiv = number % 2;
+
+        if (remDiv == 0)
+        {
+            Console.WriteLine("yes");
+
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
---
+++ homework/task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Write("enter the number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number > 1)
+    {
+      int EvenNumber = 0;
+    while (EvenNumber <= number);
+    }
+        Console.Write(EvenNumber  + " " );
+        EvenNumbers = EvenNumber + 2;
+
